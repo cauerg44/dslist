cbf7163ca57784622b602f8783b8413d59199b2d
Caue Garcia <cauerg7@outlook.com>
2023-11-17 14:19:57 -0300

    projections

--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.ents.Game;
+import com.example.dslist.projections.GameMinProjection;
 import com.example.dslist.repositories.GameRepository;
 
 @Service
@@ -29,4 +30,9 @@ public class GameService {
 		return result.stream().map(x -> new GameMinDTO(x)).toList();	
 	}
 	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();	
+	}
 }
